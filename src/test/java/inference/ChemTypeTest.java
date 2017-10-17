d0444b8f8d765b2f23b9d0356439884cd37f271f
lilott8 <lilott8@gmail.com>
2017-10-17 16:09:36 -0700

    added a few other tests.

--- src/test/java/inference/ChemTypeTest.java
+++ src/test/java/inference/ChemTypeTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import errors.CompatabilityException;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
 import static utils.CommonUtils.runTest;
 
 /**
@@ -16,20 +16,44 @@ public class ChemTypeTest {
 
     public static String root = "src/main/resources/tests/chemtype/";
 
+    /**
+     * The following tests will fail in the "mix" stage.
+     */
     @Test(expected = CompatabilityException.class)
-    public void chemType1SimpleSat() {
+    public void chemType1SimpleException() {
         String file = "chemtype1.json";
         assertFalse(runTest(root + file));
     }
 
     @Test(expected = CompatabilityException.class)
-    public void chemType2SimpleSat() {
+    public void chemType2SimpleException() {
         String file = "chemtype2.json";
         assertFalse(runTest(root + file));
     }
 
     @Test(expected = CompatabilityException.class)
-    public void chemType3SimpleSat() {
+    public void chemType3SimpleException() {
+        String file = "chemtype3.json";
+        assertFalse(runTest(root + file));
+    }
+
+    /**
+     * The following tests will fail in the SAT stage.
+     */
+    @Test
+    public void chemType1SimpleUnsat() {
+        String file = "chemtype1.json";
+        assertFalse(runTest(root + file));
+    }
+
+    @Test
+    public void chemType2SimpleUnsat() {
+        String file = "chemtype2.json";
+        assertFalse(runTest(root + file));
+    }
+
+    @Test
+    public void chemTypes3SimpleUnsat() {
         String file = "chemtype3.json";
         assertFalse(runTest(root + file));
     }
